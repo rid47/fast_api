e8a8fef7b5999e0eacc7f5f79b6669b71e9df115
Md. Ridwan Mizan <ridwanmizan@gmail.com>
2024-10-22 11:09:48 +0600

    done with the tutorial

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI,Path
 from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+
 
 employee = {
     1790: {
@@ -17,7 +22,13 @@ employee = {
     }
 }
 
+class Employee(BaseModel):
+    name: str
+    designation: str
 
+class UpdateEmployee(BaseModel):
+    name: Optional[str]=None
+    designation: Optional[str]=None
 
 
 @app.get("/home")
@@ -47,3 +58,29 @@ def get_employee(pin:int, name:Optional[str]=None):
     return employee[pin]
     
 
+@app.post("/create-employee/{employee_id}")
+def create_employee(employee_id: int, new_employee_data: Employee):
+    if employee_id in employee:
+        return {"message": "Employee already exists"}
+    employee[id] = new_employee_data
+    return new_employee_data
+
+
+@app.put("/update-employee/{employee_id}")
+def update_employee(employee_id: int, updated_employee_data: UpdateEmployee):
+    if employee_id not in employee.keys():
+        return {"message": "Employee doesn't exist!"}
+    
+    if updated_employee_data.name != None:
+        employee[employee_id]["name"] = updated_employee_data.name
+    if updated_employee_data.designation != None:
+        employee[employee_id]["designation"] = updated_employee_data.designation
+    return employee[employee_id]
+
+
+@app.delete("/delete-employee/{employee_id}")
+def delete_employee(employee_id:int):
+    if employee_id not in employee.keys():
+        return {"message": "Employee doesn't exist!"}
+    del employee[employee_id]
+    return {"message": f"Employee with id {employee_id} deleted successfully"}
